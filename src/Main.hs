24419b2022cc9f7a7ee7cbd088741b927fe1accb
Brian Gapinski <bgapinski@gmail.com>
2014-11-18 23:40:41 -0500

    Added a monster

--- src/Main.hs
+++ src/Main.hs
@@ -3,25 +3,49 @@ module Main where
 import Graphics.Rendering.OpenGL
 import Graphics.UI.GLFW (Window, Key(..), KeyState(..))
 import qualified Graphics.UI.GLFW as GLFW
+import FRP.Elerea.Simple
 
 import Control.Monad
 import Control.Monad.Fix
 import Control.Applicative
 import Control.Concurrent
-import FRP.Elerea.Simple
 
 import System.Console.GetOpt
 import System.Environment
 import System.Exit
+import System.Random
 
 type Pos = Vector2 GLdouble
 type Speed = GLdouble
-type PlayerSize = GLdouble
-data Player = Player Pos PlayerSize Speed
+type ObjectSize = GLdouble
 type Movement = (Bool, Bool, Bool, Bool)
+data Player = Player Pos ObjectSize Speed
+data Direction = WalkUp
+               | WalkDown
+               | WalkLeft
+               | WalkRight
+  deriving (Show, Eq, Enum, Bounded)
+data MonsterStatus = Wander Direction GLdouble
+                   | Hunting
+  deriving (Show)
+data Monster = Monster Pos ObjectSize Speed MonsterStatus
+
+instance Random Direction where
+  randomR (a, b) g = case randomR (fromEnum a, fromEnum b) g of
+                       (x, g') -> (toEnum x, g')
+  random g = randomR (minBound, maxBound) g
+
+wanderDist :: GLdouble
+wanderDist = 40
+
+huntingDist :: GLdouble
+huntingDist = 100
 
 initialPlayer :: Player
 initialPlayer = Player (Vector2 200 200) 20 10
+
+initialMonster :: Monster
+initialMonster = Monster (Vector2 400 400) 20 5 (Wander WalkUp wanderDist)
 
 data Options = Options
   { _optHelp :: Bool
@@ -92,9 +116,12 @@ main = do
   (dirKey, dirKeySink) <- external (False, False, False, False)
   withWindow width height title $ \win -> do
     initGL width height
+    randomGen <- newStdGen
     network <- start $ do
       player <- transfer initialPlayer (\s dk -> movePlayer width height s dk) dirKey
-      return $ renderFrame win <$> player
+      randomNumber <- stateful (undefined, randomGen) nextRandom
+      monster <- transfer2 initialMonster (wanderOrHunt width height) player randomNumber
+      return $ renderFrame win <$> player <*> monster -- <*> randomNumber
     fix $ \loop -> do
       readInput win dirKeySink
       join network
@@ -102,6 +129,8 @@ main = do
       esc <- keyIsPressed win Key'Escape
       unless esc loop
     exitSuccess
+  where
+    nextRandom (a, g) = random g
 
 initGL :: Int -> Int -> IO ()
 initGL width height = do
@@ -140,8 +169,8 @@ isPress KeyState'Pressed = True
 isPress KeyState'Repeating = True
 isPress _ = False
 
-renderFrame :: Window -> Player -> IO ()
-renderFrame window (Player (Vector2 xpos ypos) pSize _) = do
+renderFrame :: Window -> Player -> Monster -> IO ()
+renderFrame window (Player (Vector2 xpos ypos) pSize _) (Monster (Vector2 xmon ymon) monSize _ _)= do
   let half = pSize / 2
   clear [ColorBuffer]
   color $ Color4 0 1 0 (1 :: GLdouble)
@@ -150,6 +179,51 @@ renderFrame window (Player (Vector2 xpos ypos) pSize _) = do
     vertex $ Vertex2 (xpos + half) (ypos - half)
     vertex $ Vertex2 (xpos + half) (ypos + half)
     vertex $ Vertex2 (xpos - half) (ypos + half)
+  color $ Color4 1 0 0 (1 :: GLdouble)
+  renderPrimitive Triangles $ do
+    vertex $ Vertex2 (xmon - monSize / 2) (ymon - monSize / 2)
+    vertex $ Vertex2 (xmon + monSize / 2) (ymon - monSize / 2)
+    vertex $ Vertex2 xmon (ymon + monSize / 2)
   color $ Color4 1 1 1 (1 :: GLdouble)
   flush
   GLFW.swapBuffers window
+
+-- wanderOrHunt :: Player -> () -> Monster -> Monster
+wanderOrHunt width height player (randDir, _) monster =
+  if close player monster
+    then hunt player monster
+    else wander width height randDir monster
+
+close :: Player -> Monster -> Bool
+close player monster = distance player monster < huntingDist ^ 2
+
+distance :: Player -> Monster -> GLdouble
+distance (Player (Vector2 xpos ypos) _ _ ) (Monster (Vector2 xmon ymon) _ _ _) =
+  (xpos - xmon) ^ 2 + (ypos - ymon) ^ 2
+
+hunt :: Player -> Monster -> Monster
+hunt (Player (Vector2 xpos ypos) _ _) (Monster (Vector2 xmon ymon) monSize monSpeed _) =
+  Monster (Vector2 (xmon + (signum (xpos - xmon)) * monSpeed)
+                   (ymon + (signum (ypos - ymon)) * monSpeed)) monSize monSpeed Hunting
+
+wander :: Int -> Int -> Direction -> Monster -> Monster
+wander _ _ randDir (Monster (Vector2 xmon ymon) mSize mSpeed (Wander _ 0)) =
+  Monster (Vector2 xmon ymon) mSize mSpeed (Wander randDir wanderDist)
+wander _ _ randDir (Monster (Vector2 xmon ymon) mSize mSpeed Hunting) =
+  Monster (Vector2 xmon ymon) mSize mSpeed (Wander randDir wanderDist)
+wander width height _ (Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkUp n))
+  | ymon < (fromIntegral height - mSize / 2) =
+      Monster (Vector2 xmon (ymon + mSpeed)) mSize mSpeed (Wander WalkUp (n - 1))
+  | otherwise = Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkDown (n - 1))
+wander width height _ (Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkDown n))
+  | ymon > mSize / 2 =
+      Monster (Vector2 xmon (ymon - mSpeed)) mSize mSpeed (Wander WalkDown (n - 1))
+  | otherwise = Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkUp (n - 1))
+wander width height _ (Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkLeft n))
+  | xmon > mSize / 2 =
+      Monster (Vector2 (xmon - mSpeed) ymon) mSize mSpeed (Wander WalkLeft (n - 1))
+  | otherwise = Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkRight (n - 1))
+wander width height _ (Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkRight n))
+  | xmon > (fromIntegral width - mSize / 2) =
+      Monster (Vector2 (xmon + mSpeed) ymon) mSize mSpeed (Wander WalkRight (n - 1))
+  | otherwise = Monster (Vector2 xmon ymon) mSize mSpeed (Wander WalkLeft (n - 1))
